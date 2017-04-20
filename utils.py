fixed normalization

utils.py
@@ -91,7 +91,7 @@ def normalize_counts_10x(raw_counts):
     std = np.std(raw_counts, axis=0)[np.newaxis, :]
     
     #Fix potential divide by zero
-    std[np.where(std = 0)[0]] = 1
+    std[np.where(std == 0)[0]] = 1
     
     normed = (raw_counts - np.mean(raw_counts, axis=0)) / std
     # TODO: Use standardize() if we need to inverse or repeat stardardization