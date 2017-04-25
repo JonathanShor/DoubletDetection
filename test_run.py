92e1bdc16fd93d8de0d65fe111626b2a370aa27a
Adam Gayoso <adamgayoso@Adams-MacBook-Pro.local>
2017-04-24 23:01:46 -0400

    random stuff

--- test_run.py
+++ test_run.py
@@ -35,10 +35,10 @@ def main(validate):
             test = true_scores[np.where(true_scores>cutoff)[0]]
             print(cutoff, len(test))
         print(np.sum(true_doublet_labels[np.where(true_scores>0.63)[0]])/float(len(true_scores[np.where(true_scores>0.63)[0]])))
-
+        return
     else:
         # Get scores
-        counts_w_doublets, scores_w_doublets, communities_w_doublets, doublet_labels = doubletdetection.classify(raw_counts, probabilistic=True)
+        counts_w_doublets, scores_w_doublets, communities_w_doublets, doublet_labels = doubletdetection.classify(raw_counts, probabilistic=False)
         true_scores = scores_w_doublets[:raw_counts.shape[0],:]
         for s in range(20, 80, 2):
             cutoff = s/float(100)
@@ -73,9 +73,9 @@ def main(validate):
     colors = communities_w_doublets
     x = tsne_counts[:,0]
     y = tsne_counts[:,1]
-    plt.scatter(x,y, c=colors, s=10, cmap=set1i)
+    plt.scatter(x,y, c=colors, s=7, cmap=set1i)
     doublets = np.where(doublet_labels==1)[0]
-    plt.scatter(x[doublets], y[doublets], s=10, color='black')
+    plt.scatter(x[doublets], y[doublets], marker = "x", s=7, color='black')
 
 
 if __name__ == '__main__':
