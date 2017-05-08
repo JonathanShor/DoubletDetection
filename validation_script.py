b1e3a67db290e088f09208cd255bf6c854ef2cae
adamgayoso <adamgayoso@users.noreply.github.com>
2017-05-07 21:03:34 -0400

    Now added simulation

--- validation_script.py
+++ validation_script.py
@@ -14,36 +14,56 @@ import doubletdetection
 import numpy as np
 import sys
 from optparse import OptionParser
+import time
 
 
 FNAME = "~/Google Drive/Computational Genomics/5050.csv"
 
-def main():
+def main(trials):
         
     # Load data
     raw_counts = doubletdetection.load_data(FNAME)
-
-    # Classify
-    counts, scores, communities, doublet_labels, cutoff = doubletdetection.classify(raw_counts) 
-
+    
+    idxs = {}
+    for i in range(trials):
+        # Classify
+        counts, scores, communities, doublet_labels, cutoff = doubletdetection.classify(raw_counts, boost_rate=0.25) 
+        
+        doublets = np.where(scores[:raw_counts.shape[0]]>=cutoff)[0]
+        
+        for d in doublets:
+            if d not in idxs:
+                idxs[d] = 1
+            else:
+                idxs[d] += 1
+                
+            
     # Analyze doublets
-    doublets = np.where(scores[:raw_counts.shape[0]]>=cutoff)[0]
-    print('Number of doublets = ', len(doublets))
+    print("On trial number", trials, "...")
+    print('Number of doublets =', len(doublets))
     identified_doublets = raw_counts[doublets]
     XIST = np.where(identified_doublets[:,13750]>1)
     CD3D = np.where(identified_doublets[:,19801]>1)
     np.intersect1d(XIST,CD3D)
     XandC = np.intersect1d(XIST,CD3D)
-    print("Number of doublets which express both XIST and CD3D = ", len(XandC))
+    print("Number of doublets which express both XIST and CD3D =", len(XandC))
+    
+    print("\nOut of", trials, "trials there were", len(idxs.keys()), "unique doublets.")
+    print("The mean occurence of doublets is", np.mean(list(idxs.values())))
+    print("The standard deviation of occurence is", np.std(list(idxs.values())))
     
     
 if __name__ == '__main__':
     argv = sys.argv[1:]
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="file", help='read csv data from FILE', metavar='FILE')
-
+    parser.add_option("-n", type='int', dest="trials", default=10, help='number of simulation trials')
     (options, _args) = parser.parse_args()
     if options.file:
         FNAME = options.file
+    
+    start = time.time()
+    main(options.trials)    
+    end = time.time()
+    print(end - start, "seconds")
 
-    main()
