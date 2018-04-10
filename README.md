3bc065e77b80f715c15d4ada82b05f59e6f4c6a4
Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
2018-04-09 23:23:24 -0400

    v2.0 API Stablized (#81)
    * .fit -> .fit and .predict
    `.fit` now returns the fitted classifier object, with scores/p-values recorded for all iterations.
    `.predict` accepts `p_thresh` and `voter_thresh` and returns `.labels_`.
    
    * Adds attribute top_var_genes: provides the column indices of raw_counts corresponding to the genes used when a value is provided for n_top_var_genes.
    
    * Removes get_unique_genes()
    
    * Removes load_csv()
    
    * Adds normalizer parameter.
    Allows specifying the normalizing function to use on raw_counts.
    Defaults to included normalize_counts, the previously always-used function.
    
    * Rework normalize_counts
    Removes standardizeGenes option.
    Adds optional pseudocount option.
    
    * Cluster IDs (reported in attributes communities_ and synth_communities_) reflect Phenograph convention: -1 always represents cells that are not clustered, zero and up for true clusters.
    
    * Parameters knn and n_jobs removed: use phenograph_parameters to specify a non-default value.
    
    * Make voting_average public attribute.
    
    * Make all_scores_ public attribute. Removes scores_.
    
    * Unify shape of some attributes.
    `communities_`, `parents_`, `synth_communities_` shapes are now consistent regardless of `n_iters`.
    
    * Update all docstrings.
    
    * Update README.md
    
    * Update vignette notebook for 2.0 api
    
    Resolves #61 
    Resolves #73 
    
    Co-authored-by: Jonathan Shor <11262246+JonathanShor@users.noreply.github.com>
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 DoubletDetection is a Python3 package to detect doublets (technical errors) in single-cell RNA-seq count matrices.
 
-To install DoubletDetection, clone or download this repository and run.
+To install DoubletDetection:
 
-```bash
+```
+git clone https://github.com/JonathanShor/DoubletDetection.git
+cd DoubletDetection
 pip3 install --upgrade .
 ```
 
@@ -12,15 +14,16 @@ To run basic doublet classification:
 
 ```
 import doubletdetection
-raw_counts = doubletdetection.load_csv('/your/path/filename.csv')
 clf = doubletdetection.BoostClassifier()
-labels = clf.fit(raw_counts)
+# raw_counts is a cells by genes count matrix
+labels = clf.fit(raw_counts).predict()
 ```
 
 `raw_counts` is a scRNA-seq count matrix, and must be a 2-dimensional numpy ndarray, with rows 
 being cells. `labels` is a binary 1-dimensional numpy ndarray with the value 1 representing a 
 detected doublet. The length of `labels` is equal to the length of `raw_counts`.
 
+See our [jupyter notebook](https://nbviewer.jupyter.org/github/JonathanShor/DoubletDetection/blob/master/docs/PBMC_8k_vignette.ipynb) for an example on 8k PBMCs from 10x.
 
 ## Obtaining data
 Data can be downloaded from the [10x website](https://support.10xgenomics.com/single-cell/datasets).
--- src/doubletdetection/doubletdetection.py
+++ src/doubletdetection/doubletdetection.py
@@ -2,13 +2,33 @@
 
 import collections
 import warnings
-import logging
 
 import numpy as np
-import pandas as pd
 import phenograph
 from sklearn.decomposition import PCA
 from scipy.stats import hypergeom
+
+
+def normalize_counts(raw_counts, pseudocount=0.1):
+    """Normalize count array.
+
+    Args:
+        raw_counts (ndarray): count data
+        pseudocount (float, optional): Count to add prior to log transform.
+
+    Returns:
+        ndarray: Normalized data.
+    """
+    # Sum across cells
+    cell_sums = np.sum(raw_counts, axis=1)
+
+    # Mutiply by median and divide each cell by cell sum
+    median = np.median(cell_sums)
+    normed = raw_counts * median / cell_sums[:, np.newaxis]
+
+    normed = np.log(normed + pseudocount)
+
+    return normed
 
 
 class BoostClassifier:
@@ -17,71 +37,63 @@ class BoostClassifier:
     Parameters:
         boost_rate (float, optional): Proportion of cell population size to
             produce as synthetic doublets.
-        knn (int, optional): Number of nearest neighbors used in Phenograph
-            clustering. Ignored if 'k' specified in phenograph_parameters.
-        n_pca (int, optional): Number of PCA components used for clustering.
+        n_components (int, optional): Number of principal components used for
+            clustering.
         n_top_var_genes (int, optional): Number of highest variance genes to
-            use; other genes discarded. Will use all genes when non-positive.
+            use; other genes discarded. Will use all genes when zero.
         new_lib_as: (([int, int]) -> int, optional): Method to use in choosing
-            new library size for boosts. Defaults to np.mean. A common
-            alternative is new_lib_as=max.
-        replace (bool, optional): If true, creates boosts by choosing parents
-            with replacement
-        n_jobs (int, optional): Number of cores to use. Default is -1: all
-            available.
-        phenograph_parameters (dict, optional): Phenograph parameters to
-            override and their corresponding requested values.
-        n_iters (int, optional): (Recommended value is n_iters=5, and will
-            likely be set in a future release.) Number of fit operations from
-            which to produce p-values. More runs produce more robust p-values.
-            NOTE: that most informational attributes will be set to None when
-            running more than once.
+            library size for synthetic doublets. Defaults to np.max; append
+            alternative is new_lib_as=np.mean.
+        replace (bool, optional): If False, a cell will be selected as a
+            synthetic doublet's parent no more than once.
+        phenograph_parameters (dict, optional): Parameter dict to pass directly
+            to Phenograph.
+        n_iters (int, optional): Number of fit operations from which to collect
+            p-values. Defualt value is 25.
+        normalizer ((ndarray) -> ndarray): Method to normalize raw_counts.
+            Defaults to normalize_counts, included in this package. Note: To use
+            normalize_counts with its pseudocount parameter changed from the
+            default 0.1 value to some positive float `new_var`, use:
+            normalizer=lambda counts: doubletdetection.normalize_counts(counts,
+            pseudocount=new_var)
 
     Attributes:
-        communities_ (ndarray): Cluster ID for corresponding cell. 2D ndarary
-            when n_iters > 1, with shape (n_iters, num_cells).
+        all_p_values_ (ndarray): Hypergeometric test p-value per cell for cluster
+            enrichment of synthetic doublets. Shape (n_iters, num_cells).
+        all_scores_ (ndarray): The fraction of a cell's cluster that is
+            synthetic doublets. Shape (n_iters, num_cells).
+        communities_ (ndarray): Cluster ID for corresponding cell. Shape
+            (n_iters, num_cells).
         labels_ (ndarray, ndims=1): 0 for singlet, 1 for detected doublet.
         parents_ (list of sequences of int): Parent cells' indexes for each
-            synthetic doublet. When n_iters > 1, this is a list wrapping the
-            results from each run.
-        raw_synthetics_ (ndarray, ndims=2): Raw counts of synthetic doublets.
-            Not produced when n_iters > 1.
-        scores_ (ndarray): Doublet score for each cell. This is the mean across
-            all runs when n_iter > 1.
-        p_values_ (ndarray): Mean hypergeometric test value across n_iters runs
-             for each cell.
-        suggested_cutoff_ (float): Recommended cutoff to use (scores_ >= cutoff).
-            Not produced when n_iters > 1.
+            synthetic doublet. A list wrapping the results from each run.
+        suggested_score_cutoff_ (float): Cutoff used to classify cells when
+            n_iters == 1 (scores >= cutoff). Not produced when n_iters > 1.
         synth_communities_ (sequence of ints): Cluster ID for corresponding
-            synthetic doublet. 2D ndarary when n_iters > 1, with shape
-            (n_iters, num_cells * boost_rate).
+            synthetic doublet. Shape (n_iters, num_cells * boost_rate).
+        top_var_genes_ (ndarray): Indices of the n_top_var_genes used. Not
+            generated if n_top_var_genes <= 0.
+        voting_average_ (ndarray): Fraction of iterations each cell is called a
+            doublet.
     """
 
-    def __init__(self, boost_rate=0.25, knn=30, n_pca=30, n_top_var_genes=10000, new_lib_as=np.max,
-                 replace=False, n_jobs=-1, phenograph_parameters={'prune': True}, n_iters=25):
+    def __init__(self, boost_rate=0.25, n_components=30, n_top_var_genes=10000, new_lib_as=np.max,
+                 replace=False, phenograph_parameters={'prune': True}, n_iters=25,
+                 normalizer=normalize_counts):
         self.boost_rate = boost_rate
         self.new_lib_as = new_lib_as
         self.replace = replace
-        self.n_jobs = n_jobs
         self.n_iters = n_iters
+        self.normalizer = normalizer
 
-        if n_pca == 30 and n_top_var_genes > 0:
-            # If user did not change n_pca, silently cap it by n_top_var_genes if needed
-            self.n_pca = min(n_pca, n_top_var_genes)
+        if n_components == 30 and n_top_var_genes > 0:
+            # If user did not change n_components, silently cap it by n_top_var_genes if needed
+            self.n_components = min(n_components, n_top_var_genes)
         else:
-            self.n_pca = n_pca
+            self.n_components = n_components
         # Floor negative n_top_var_genes by 0
         self.n_top_var_genes = max(0, n_top_var_genes)
 
-        if phenograph_parameters:
-            if 'k' not in phenograph_parameters:
-                phenograph_parameters['k'] = knn
-            else:
-                logging.info("Ignoring 'knn' parameter, as 'k' provided in phenograph_parameters.")
-            if 'n_jobs' not in phenograph_parameters:
-                phenograph_parameters['n_jobs'] = n_jobs
-        else:
-            phenograph_parameters = {'k': knn, 'n_jobs': n_jobs}
         self.phenograph_parameters = phenograph_parameters
         if (self.n_iters == 1) and (phenograph_parameters.get('prune') is True):
             warn_msg = ("Using phenograph parameter prune=False is strongly recommended when " +
@@ -94,95 +106,109 @@ class BoostClassifier:
             warnings.warn(warn_msg)
             self.boost_rate = 0.5
 
-        assert (self.n_top_var_genes == 0) or (self.n_pca <= self.n_top_var_genes), (
-            "n_pca={0} cannot be larger than n_top_var_genes={1}".format(n_pca, n_top_var_genes))
+        assert (self.n_top_var_genes == 0) or (self.n_components <= self.n_top_var_genes), (
+            "n_components={0} cannot be larger than n_top_var_genes={1}".format(n_components,
+                                                                                n_top_var_genes))
 
-    def fit(self, raw_counts, p_thresh=0.99, voter_thresh=0.9):
-        """Identify doublets in single-cell RNA-seq count table raw_counts.
+    def fit(self, raw_counts):
+        """Fits the classifier on raw_counts.
 
         Args:
             raw_counts (ndarray): Count table, oriented cells by genes.
 
         Sets:
-            communities_, parents_ , raw_synthetics_, scores_, suggested_cutoff_
+            all_scores_, all_p_values_, communities_, parents_,
+            synth_communities
 
         Returns:
-            labels_ (ndarray, ndims=1):  0 for singlet, 1 for detected doublet
+            The fitted classifier.
         """
         if self.n_top_var_genes > 0:
             if self.n_top_var_genes < raw_counts.shape[1]:
                 gene_variances = np.var(raw_counts, axis=0)
                 top_var_indexes = np.argsort(gene_variances)
-                top_var_indexes = top_var_indexes[-self.n_top_var_genes:]
-                raw_counts = raw_counts[:, top_var_indexes]
+                self.top_var_genes_ = top_var_indexes[-self.n_top_var_genes:]
+                raw_counts = raw_counts[:, self.top_var_genes_]
 
         self._raw_counts = raw_counts
         (self._num_cells, self._num_genes) = self._raw_counts.shape
 
-        self._all_scores = np.zeros((self.n_iters, self._num_cells))
-        self._all_p_values = np.zeros((self.n_iters, self._num_cells))
+        self.all_scores_ = np.zeros((self.n_iters, self._num_cells))
+        self.all_p_values_ = np.zeros((self.n_iters, self._num_cells))
         all_communities = np.zeros((self.n_iters, self._num_cells))
         all_parents = []
         all_synth_communities = np.zeros((self.n_iters, int(self.boost_rate * self._num_cells)))
 
         for i in range(self.n_iters):
-            self._all_scores[i], self._all_p_values[i] = self._one_fit()
-            if self.n_iters > 1:
-                all_communities[i] = self.communities_
-                all_parents.append(self.parents_)
-                all_synth_communities[i] = self.synth_communities_
+            self.all_scores_[i], self.all_p_values_[i] = self._one_fit()
+            all_communities[i] = self.communities_
+            all_parents.append(self.parents_)
+            all_synth_communities[i] = self.synth_communities_
 
-        # NaNs correspond to unclustered cells. Ignore those runs.
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always", category=RuntimeWarning)
-            self.scores_ = np.nanmean(self._all_scores, axis=0)
-            self.p_values_ = np.nanmean(self._all_p_values, axis=0)
-        if w:
-            warnings.warn("One or more cells failed to join a cluster across all runs.",
-                          category=RuntimeWarning)
+        # Release unneeded large data vars
+        del self._raw_counts
+        del self._norm_counts
+        del self._raw_synthetics
+        del self._synthetics
 
+        self.communities_ = all_communities
+        self.parents_ = all_parents
+        self.synth_communities_ = all_synth_communities
+
+        return self
+
+    def predict(self, p_thresh=0.99, voter_thresh=0.9):
+        """Produce doublet calls from fitted classifier
+
+        Args:
+            p_thresh (float, optional): hypergeometric test p-value threshold
+                that determines per iteration doublet calls
+            voter_thresh (float, optional): fraction of iterations a cell must
+                be called a doublet
+
+        Sets:
+            labels_ and voting_average_ if n_iters > 1.
+            labels_ and suggested_score_cutoff_ if n_iters == 1.
+
+        Returns:
+            labels_ (ndarray, ndims=1):  0 for singlet, 1 for detected doublet
+        """
         if self.n_iters > 1:
-            self.communities_ = all_communities
-            self.parents_ = all_parents
-            self.synth_communities_ = all_synth_communities
-            del self.raw_synthetics_
             with np.errstate(invalid='ignore'):  # Silence numpy warning about NaN comparison
-                self._voting_average = np.mean(np.ma.masked_invalid(self._all_p_values) > p_thresh,
+                self.voting_average_ = np.mean(np.ma.masked_invalid(self.all_p_values_) > p_thresh,
                                                axis=0)
-                self.labels_ = np.ma.filled(self._voting_average >= voter_thresh, np.nan)
+                self.labels_ = np.ma.filled(self.voting_average_ >= voter_thresh, np.nan)
+                self.voting_average_ = np.ma.filled(self.voting_average_, np.nan)
         else:
             # Find a cutoff score
-            potential_cutoffs = np.unique(self.scores_)
+            potential_cutoffs = np.unique(self.all_scores_[~np.isnan(self.all_scores_)])
             if len(potential_cutoffs) > 1:
                 max_dropoff = np.argmax(potential_cutoffs[1:] - potential_cutoffs[:-1]) + 1
             else:   # Most likely pathological dataset, only one (or no) clusters
                 max_dropoff = 0
-            self.suggested_cutoff_ = potential_cutoffs[max_dropoff]
+            self.suggested_score_cutoff_ = potential_cutoffs[max_dropoff]
             with np.errstate(invalid='ignore'):  # Silence numpy warning about NaN comparison
-                self.labels_ = self.scores_ >= self.suggested_cutoff_
+                self.labels_ = self.all_scores_[0, :] >= self.suggested_score_cutoff_
 
         return self.labels_
 
     def _one_fit(self):
         print("\nCreating downsampled doublets...")
-        self._createLinearDoublets()
+        self._createDoublets()
 
         # Normalize combined augmented set
         print("Normalizing...")
-        aug_counts = normalize_counts(np.append(self._raw_counts, self.raw_synthetics_, axis=0))
+        aug_counts = self.normalizer(np.append(self._raw_counts, self._raw_synthetics, axis=0))
         self._norm_counts = aug_counts[:self._num_cells]
         self._synthetics = aug_counts[self._num_cells:]
 
         print("Running PCA...")
         # Get phenograph results
-        pca = PCA(n_components=self.n_pca)
+        pca = PCA(n_components=self.n_components)
         print("Clustering augmented data set with Phenograph...\n")
         reduced_counts = pca.fit_transform(aug_counts)
         fullcommunities, _, _ = phenograph.cluster(reduced_counts, **self.phenograph_parameters)
         min_ID = min(fullcommunities)
-        if min_ID < 0:
-            logging.debug("Adjusting community IDs up {} to avoid negative.".format(abs(min_ID)))
-            fullcommunities = fullcommunities + abs(min_ID)
         self.communities_ = fullcommunities[:self._num_cells]
         self.synth_communities_ = fullcommunities[self._num_cells:]
         community_sizes = [np.count_nonzero(fullcommunities == i)
@@ -195,21 +221,21 @@ class BoostClassifier:
         # Number of synth/orig cells in each cluster.
         synth_cells_per_comm = collections.Counter(self.synth_communities_)
         orig_cells_per_comm = collections.Counter(self.communities_)
-        community_IDs = sorted(synth_cells_per_comm | orig_cells_per_comm)
-        community_scores = [float(synth_cells_per_comm[i]) /
+        community_IDs = orig_cells_per_comm.keys()
+        community_scores = {i: float(synth_cells_per_comm[i]) /
                             (synth_cells_per_comm[i] + orig_cells_per_comm[i])
-                            for i in community_IDs]
+                            for i in community_IDs}
         scores = np.array([community_scores[i] for i in self.communities_])
 
-        community_p_values = [hypergeom.cdf(synth_cells_per_comm[i], aug_counts.shape[0],
+        community_p_values = {i: hypergeom.cdf(synth_cells_per_comm[i], aug_counts.shape[0],
                                             self._synthetics.shape[0],
                                             synth_cells_per_comm[i] + orig_cells_per_comm[i])
-                              for i in community_IDs]
+                              for i in community_IDs}
         p_values = np.array([community_p_values[i] for i in self.communities_])
 
         if min_ID < 0:
-            scores[self.communities_ == 0] = np.nan
-            p_values[self.communities_ == 0] = np.nan
+            scores[self.communities_ == -1] = np.nan
+            p_values[self.communities_ == -1] = np.nan
 
         return scores, p_values
 
@@ -235,10 +261,10 @@ class BoostClassifier:
 
         return new_cell
 
-    def _createLinearDoublets(self):
+    def _createDoublets(self):
         """Create synthetic doublets.
 
-        Sets .raw_synthetics_ and .parents_
+        Sets .parents_
         """
         # Number of synthetic doublets to add
         num_synths = int(self.boost_rate * self._num_cells)
@@ -254,89 +280,5 @@ class BoostClassifier:
             synthetic[i] = new_row
             parents.append([row1, row2])
 
-        self.raw_synthetics_ = synthetic
+        self._raw_synthetics = synthetic
         self.parents_ = parents
-
-
-def get_unique_genes(raw_counts, communities):
-    """Identify (any) genes unique to each community.
-
-    Args:
-        raw_counts (ndarray, ndims=2): Cell x genes counts numpy array.
-        communities (ndarray, shape=(raw_counts.shape[0],)): Community ID for
-            each cell.
-
-    Returns:
-        ndarray, dtype=int: 1 for each gene unique to that community.
-    """
-    # Sum each community's genecounts, and stack up those gene profile vectors
-    profiles = np.concatenate([np.sum(raw_counts[communities == i], axis=0, keepdims=True) for i in
-                               np.unique(communities)], axis=0)
-
-    binary = np.zeros_like(profiles)
-    binary[profiles != 0] = 1
-
-    # Only 1 - sum(everything) + 1 > 0
-    uniques = binary - np.sum(binary, axis=0) + binary
-    uniques[uniques < 0] = 0
-
-    return uniques
-
-
-def load_csv(FNAME, normalize=False, read_rows=None):
-    """Load a csv table from the filesystem.
-
-    Note that the csv's first row and first column are assumed to be labels and
-    are discarded.
-
-    Args:
-        FNAME (string): Pathname of file to load.
-        normalize (bool, optional): Runs normalize_counts on table.
-        read_rows (None or int, optional): If specified, load only first
-            read_rows of data from FNAME.
-
-    Returns:
-        ndarray: Loaded table.
-    """
-    logging.info("Loading {}".format(FNAME))
-    counts = pd.read_csv(FNAME, index_col=0, nrows=read_rows).as_matrix()
-
-    if normalize:
-        counts = normalize_counts(counts)
-
-    return counts
-
-
-def normalize_counts(raw_counts, standardizeGenes=False):
-    """Normalize count array using method in 10x pipeline.
-
-    From http://www.nature.com/articles/ncomms14049.
-
-    Args:
-        raw_counts (ndarray): count data
-        standardizeGenes (bool, optional): Standardizes each gene column.
-
-    Returns:
-        ndarray: Normalized data.
-    """
-    # Sum across cells
-    cell_sums = np.sum(raw_counts, axis=1)
-
-    # Mutiply by median and divide each cell by cell sum
-    median = np.median(cell_sums)
-    raw_counts = raw_counts * median / cell_sums[:, np.newaxis]
-
-    raw_counts = np.log(raw_counts + 0.1)
-
-    if standardizeGenes:
-        # Normalize to have genes with mean 0 and std 1
-        std = np.std(raw_counts, axis=0)[np.newaxis, :]
-
-        # Fix potential divide by zero
-        std[np.where(std == 0)[0]] = 1
-
-        normed = (raw_counts - np.mean(raw_counts, axis=0)) / std
-    else:
-        normed = raw_counts
-
-    return normed
