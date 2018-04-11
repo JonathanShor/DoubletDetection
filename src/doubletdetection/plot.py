b7ac396f0324185ea38906c078ec706703aa28a1
adamgayoso <adamgayoso@users.noreply.github.com>
2018-04-11 18:10:43 -0400

    Added tsne function
    Doublets marked in black, example shown in updated notebook

--- src/doubletdetection/plot.py
+++ src/doubletdetection/plot.py
@@ -1,3 +1,8 @@
+from .doubletdetection import normalize_counts
+from sklearn.decomposition import PCA
+import bhtsne
+import phenograph
+
 import os
 import warnings
 from cycler import cycler
@@ -13,12 +18,14 @@ except KeyError:
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 with warnings.catch_warnings():
-    warnings.simplefilter('ignore')  # catch warnings that system can't find fonts
+    # catch warnings that system can't find fonts
+    warnings.simplefilter('ignore')
     fm = font_manager.fontManager
     fm.findfont('Raleway')
     fm.findfont('Lato')
 
-warnings.filterwarnings(action="ignore", module="matplotlib", message="^tight_layout")
+warnings.filterwarnings(
+    action="ignore", module="matplotlib", message="^tight_layout")
 
 dark_gray = '.15'
 
@@ -77,7 +84,8 @@ style_dictionary = {
 
 matplotlib.rcParams.update(style_dictionary)
 
-def convergence(clf, show=True, save=False, p_thresh=0.99, voter_thresh=0.9):
+
+def convergence(clf, show=False, save=False, p_thresh=0.99, voter_thresh=0.9):
     """Produce a plot showing number of cells called doublet per iter
 
     Args:
@@ -92,8 +100,9 @@ def convergence(clf, show=True, save=False, p_thresh=0.99, voter_thresh=0.9):
     """
     doubs_per_run = []
     for i in range(clf.n_iters):
-        cum_p_values = clf.all_p_values_[:i+1]
-        cum_vote_average = np.mean(np.ma.masked_invalid(cum_p_values) > p_thresh, axis=0)
+        cum_p_values = clf.all_p_values_[:i + 1]
+        cum_vote_average = np.mean(
+            np.ma.masked_invalid(cum_p_values) > p_thresh, axis=0)
         cum_doublets = np.ma.filled(cum_vote_average >= voter_thresh, np.nan)
         doubs_per_run.append(np.sum(cum_doublets))
 
@@ -107,3 +116,23 @@ def convergence(clf, show=True, save=False, p_thresh=0.99, voter_thresh=0.9):
         plt.show()
     if save is True:
         plt.savefig('doublet_convergence.pdf', format='pdf')
+
+
+def tsne(raw_counts, labels, n_components=30):
+
+    norm_counts = normalize_counts(raw_counts)
+    reduced_counts = PCA(n_components=n_components,
+                         svd_solver='randomized').fit_transform(norm_counts)
+    communities, _, _ = phenograph.cluster(reduced_counts)
+    tsne_counts = bhtsne.tsne(reduced_counts, rand_seed=1)
+
+    fig, axes = plt.subplots(1, 1)
+    axes.scatter(tsne_counts[:, 0], tsne_counts[:, 1],
+                 c=communities, cmap=plt.cm.tab20, s=1)
+    axes.scatter(tsne_counts[:, 0][labels], tsne_counts[:, 1]
+                 [labels], s=3, edgecolor='k', facecolor='k')
+    axes.set_title('Cells with Detected\n Doublets in Black')
+    plt.xticks([])
+    plt.yticks([])
+    axes.set_xlabel('{} out of {} cells called doublets.\n {}%  across-type doublet rate.'.format(
+        np.sum(labels), raw_counts.shape[0], np.round(100 * np.sum(labels) / raw_counts.shape[0], 2)))
