e5aa819e77fefa9172dbd3ce48264151f5efa1b0
adamgayoso <adamgayoso@users.noreply.github.com>
2019-06-27 09:49:10 -0700

    Converts the codebase to the black code style

--- src/doubletdetection/plot.py
+++ src/doubletdetection/plot.py
@@ -8,10 +8,11 @@ import numpy as np
 from sklearn.utils import check_array
 
 import matplotlib
+
 try:
-    os.environ['DISPLAY']
+    os.environ["DISPLAY"]
 except KeyError:
-    matplotlib.use('Agg')
+    matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 
@@ -57,11 +58,12 @@ def convergence(clf, show=False, save=None, p_thresh=1e-7, voter_thresh=0.9):
     log_p_thresh = np.log(p_thresh)
     doubs_per_run = []
     # Ignore numpy complaining about np.nan comparisons
-    with np.errstate(invalid='ignore'):
+    with np.errstate(invalid="ignore"):
         for i in range(clf.n_iters):
-            cum_log_p_values = clf.all_log_p_values_[:i + 1]
-            cum_vote_average = np.mean(np.ma.masked_invalid(cum_log_p_values) <= log_p_thresh,
-                                       axis=0)
+            cum_log_p_values = clf.all_log_p_values_[: i + 1]
+            cum_vote_average = np.mean(
+                np.ma.masked_invalid(cum_log_p_values) <= log_p_thresh, axis=0
+            )
             cum_doublets = np.ma.filled((cum_vote_average >= voter_thresh).astype(float), np.nan)
             doubs_per_run.append(np.nansum(cum_doublets))
 
@@ -73,18 +75,25 @@ def convergence(clf, show=False, save=None, p_thresh=1e-7, voter_thresh=0.9):
         ax.plot(np.arange(len(doubs_per_run)), doubs_per_run)
         ax.set_xlabel("Number of Iterations")
         ax.set_ylabel("Number of Predicted Doublets")
-        ax.set_title('Predicted Doublets per Iteration')
+        ax.set_title("Predicted Doublets per Iteration")
 
         if show is True:
             plt.show()
         if isinstance(save, str):
-            f.savefig(save, format='pdf', bbox_inches='tight')
+            f.savefig(save, format="pdf", bbox_inches="tight")
 
     return f
 
 
-def umap_plot(raw_counts, labels, n_components=30, show=False, save=None,
-         normalizer=normalize_counts, random_state=None):
+def umap_plot(
+    raw_counts,
+    labels,
+    n_components=30,
+    show=False,
+    save=None,
+    normalizer=normalize_counts,
+    random_state=None,
+):
     """Produce a umap plot of the data with doublets in black.
 
         Count matrix is normalized and dimension reduced before plotting.
@@ -111,41 +120,47 @@ def umap_plot(raw_counts, labels, n_components=30, show=False, save=None,
     """
 
     try:
-        raw_counts = check_array(raw_counts, accept_sparse=False, force_all_finite=True,
-                                 ensure_2d=True)
-    except TypeError:   # Only catches sparse error. Non-finite & n_dims still raised.
+        raw_counts = check_array(
+            raw_counts, accept_sparse=False, force_all_finite=True, ensure_2d=True
+        )
+    except TypeError:  # Only catches sparse error. Non-finite & n_dims still raised.
         warnings.warn("Sparse raw_counts is automatically densified.")
         raw_counts = raw_counts.toarray()
     norm_counts = normalizer(raw_counts)
-    reduced_counts = PCA(n_components=n_components,
-                         svd_solver='randomized', random_state=random_state).fit_transform(norm_counts)
+    reduced_counts = PCA(
+        n_components=n_components, svd_solver="randomized", random_state=random_state
+    ).fit_transform(norm_counts)
     communities, _, _ = phenograph.cluster(reduced_counts)
     umap_dr = umap.UMAP(random_state=random_state, min_dist=0.5).fit_transform(reduced_counts)
     # Ensure only looking at positively identified doublets
     doublets = labels == 1
 
     fig, axes = plt.subplots(1, 1, figsize=(4, 4), dpi=150)
-    axes.scatter(umap_dr[:, 0], umap_dr[:, 1],
-                 c=communities, cmap=plt.cm.tab20, s=1)
-    axes.scatter(umap_dr[:, 0][doublets], umap_dr[:, 1][doublets],
-                 s=3, c='black', label='predictions')
-    axes.axis('off')
+    axes.scatter(umap_dr[:, 0], umap_dr[:, 1], c=communities, cmap=plt.cm.tab20, s=1)
+    axes.scatter(
+        umap_dr[:, 0][doublets], umap_dr[:, 1][doublets], s=3, c="black", label="predictions"
+    )
+    axes.axis("off")
     axes.legend(frameon=False)
-    axes.set_title('{} doublets out of {} cells\n {}% cross-type doublet rate'.format(
-        np.sum(doublets),
-        raw_counts.shape[0],
-        np.round(100 * np.sum(doublets) / raw_counts.shape[0], 2)))
+    axes.set_title(
+        "{} doublets out of {} cells\n {}% cross-type doublet rate".format(
+            np.sum(doublets),
+            raw_counts.shape[0],
+            np.round(100 * np.sum(doublets) / raw_counts.shape[0], 2),
+        )
+    )
 
     if show is True:
         plt.show()
     if isinstance(save, str):
-        fig.savefig(save, format='pdf', bbox_inches='tight')
+        fig.savefig(save, format="pdf", bbox_inches="tight")
 
     return fig, umap_dr, communities
 
 
-def threshold(clf, show=False, save=None, log10=True, log_p_grid=None, voter_grid=None, v_step=2,
-              p_step=5):
+def threshold(
+    clf, show=False, save=None, log10=True, log_p_grid=None, voter_grid=None, v_step=2, p_step=5
+):
     """Produce a plot showing number of cells called doublet across
        various thresholds
 
@@ -167,7 +182,7 @@ def threshold(clf, show=False, save=None, log10=True, log_p_grid=None, voter_gri
         matplotlib figure
     """
     # Ignore numpy complaining about np.nan comparisons
-    with np.errstate(invalid='ignore'):
+    with np.errstate(invalid="ignore"):
         all_log_p_values_ = np.copy(clf.all_log_p_values_)
         if log10:
             all_log_p_values_ /= np.log(10)
@@ -178,8 +193,9 @@ def threshold(clf, show=False, save=None, log10=True, log_p_grid=None, voter_gri
         doubs_per_t = np.zeros((len(voter_grid), len(log_p_grid)))
         for i in range(len(voter_grid)):
             for j in range(len(log_p_grid)):
-                voting_average = np.mean(np.ma.masked_invalid(all_log_p_values_) <= log_p_grid[j],
-                                         axis=0)
+                voting_average = np.mean(
+                    np.ma.masked_invalid(all_log_p_values_) <= log_p_grid[j], axis=0
+                )
                 labels = np.ma.filled((voting_average >= voter_grid[i]).astype(float), np.nan)
                 doubs_per_t[i, j] = np.nansum(labels)
 
@@ -188,23 +204,23 @@ def threshold(clf, show=False, save=None, log10=True, log_p_grid=None, voter_gri
         warnings.filterwarnings(action="ignore", module="matplotlib", message="^tight_layout")
 
         f, ax = plt.subplots(1, 1, figsize=(4, 4), dpi=150)
-        cax = ax.imshow(doubs_per_t, cmap='hot', aspect='auto')
+        cax = ax.imshow(doubs_per_t, cmap="hot", aspect="auto")
         ax.set_xticks(np.arange(len(log_p_grid))[::p_step])
-        ax.set_xticklabels(np.around(log_p_grid, 1)[::p_step], rotation='vertical')
+        ax.set_xticklabels(np.around(log_p_grid, 1)[::p_step], rotation="vertical")
         ax.set_yticks(np.arange(len(voter_grid))[::v_step])
         ax.set_yticklabels(np.around(voter_grid, 2)[::v_step])
         cbar = f.colorbar(cax)
-        cbar.set_label('Predicted Doublets')
+        cbar.set_label("Predicted Doublets")
         if log10 is True:
-            ax.set_xlabel('Log10 p-value')
+            ax.set_xlabel("Log10 p-value")
         else:
-            ax.set_xlabel('Log p-value')
+            ax.set_xlabel("Log p-value")
         ax.set_ylabel("Voting Threshold")
-        ax.set_title('Threshold Diagnostics')
+        ax.set_title("Threshold Diagnostics")
 
     if show is True:
         plt.show()
     if save:
-        f.savefig(save, format='pdf', bbox_inches='tight')
+        f.savefig(save, format="pdf", bbox_inches="tight")
 
     return f
