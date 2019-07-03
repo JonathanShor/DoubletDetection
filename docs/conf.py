0eccdee8cbf087632a4c2fa2f3991db64654618c
adamgayoso <adamgayoso@users.noreply.github.com>
2019-07-03 16:40:58 -0700

    correct version number in conf

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Adam Gayoso and Jonathan Shor'
 author = 'Adam Gayoso and Jonathan Shor'
 
 # The full version, including alpha/beta/rc tags
-release = '2.4.1'
+release = '2.4.0'
 
 
 # -- General configuration ---------------------------------------------------
